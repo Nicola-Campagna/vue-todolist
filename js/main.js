c59871854c50ffe66bdf5bf9c4fdc6e07e5a56b6
Nicola-Campagna <campagnanicola379@gmail.com>
2023-02-13 18:08:12 +0100

    add todo list+function check! and remove

--- js/main.js
+++ js/main.js
@@ -18,8 +18,42 @@
 const settVue = Vue.createApp({
     data() {
         return {
+            // ogni todo sarà un oggetto, formato da due proprietà:
+            todo: {
+                tasks: [
+                    "sono la task 1",
+                    "sono la task 2",
+                    "sono la task 3",
+                    "sono la task 4",
+                    "sono la task 5",
+                ],
+
+                done: [
+                    false,
+                    false,
+                    false,
+                    false,
+                    false,
+                ],
+            },
+
+
 
         }
+
+    },
+
+    methods: {
+        checkTask(i) {
+            this.todo.done = true;
+            console.log("task completa" + (i + 1))
+
+        },
+        removeTask(i) {
+            this.todo.tasks.splice(i, 1);
+            console.log("cliccato la task" + (i + 1))
+        }
+
     }
 })
 settVue.mount("#root")
